157472c789247ce1dd3f9d1b28b2c968c3bbaf5d
Axel Beaulieu <axel.b.luangkham@gmail.com>
2025-05-26 00:21:18 +0200

    feat: cleaned the first controller, now ready for use, need to link the view, not even started yet, seems ready to go

--- website/controller/faction_controller.php
+++ website/controller/faction_controller.php
@@ -1,29 +1,49 @@
 <?php
-include_once(__DIR__."/model/faction_model.php");
+include_once __DIR__."/../model/faction_model.php";
 
 class Faction_controller {
-    private $controller;
+    private $pdo;
 
     public function __construct() {
-        $this->controller = new Faction_model();
+        $this->pdo = new Faction_model();
     }
 
     public function get_all_factions() {
-        $tab_factions = $this->controller->get_factions();
+        $tab_factions = $this->pdo->get_factions();
         return $tab_factions;
     }
     
     public function this_faction($name) {
-        $faction = $this->controller->get_faction_from_name($name);
+        $faction = $this->pdo->get_faction_from_name($name);
         return $faction;
     }
 
-    public function get_faction_name($name) {
-        $faction = $this->controller->get_faction_from_name($name);
-        $faction_name = $faction["nom"];
-        return $faction_name;
+    public function get_factions_belonging_to($appartenance) {
+        $faction = $this->pdo->get_faction_from_appartenance($appartenance);
+        return $faction;
+    }
+
+    public function get_factions_with_no_appartenance() {
+        $all_factions = $this->pdo->get_factions();
+        $root_factions = array();
+        foreach ($all_factions as $faction) {
+            if ($faction["appartenance"] == NULL) {
+                $faction_name = $faction["nom"];
+                $root_factions[$faction_name] = $faction;
+            }
+        }
+        return $root_factions;
     }
 }
 
-  
+$controller = new Faction_controller();
+
+$main_factions = array();
+$main_factions = $controller->get_factions_with_no_appartenance();
+
+$all_factions = $controller->get_all_factions();
+
+include ""; //vue;
+include ""; //vue;
+include ""; //vue;
 ?>
